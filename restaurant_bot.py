c54ff5d9126015a21f79dc7c583f1aa3d87b7e27
Keith <60371048+keith-letourneau@users.noreply.github.com>
2020-11-05 12:55:24 -0500

    Update restaurant_bot.py

--- restaurant_bot.py
+++ restaurant_bot.py
@@ -11,19 +11,15 @@ def eatery():
   """
 
 #User input for search query
-  food_type = input("Hello! I am the restaurant_bot and I can recommend you a highly rated place to eat!\n\nI have only one rule: no spaces are allowed in your answers!\n\nWhat do you feel like eating? (Ex. Korean-BBQ) ")
-
+  food_type = input("Hello! I am the restaurant_bot and I can recommend you a highly rated place to eat!\n\nI have only one rule: no spaces are allowed in your answers!
+                    \n\nWhat do you feel like eating? (Ex. Korean-BBQ) ")
   location = input("\nWhere are you located? (Ex. San-Francisco) ")
-
   results = ('https://www.yelp.com/search?find_desc=' + food_type + '&find_loc=' + location + '&sortby=review_count')
  
 #Web scrape query page and select the URL for the top rated option
   parser = 'html.parser'
-
   resp = urllib.request.urlopen(results)
-
   soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))
-
   eatery_list = []
 
   for link in soup.select("a[href*='/biz']"):
