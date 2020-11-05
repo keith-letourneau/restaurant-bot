a19e7f592777cb9ce5015d1dcc15da9395af7438
Keith <60371048+keith-letourneau@users.noreply.github.com>
2020-11-05 12:58:18 -0500

    Update restaurant_bot.py

--- restaurant_bot.py
+++ restaurant_bot.py
@@ -1,5 +1,4 @@
-#restaurant_bot
-
+#import libraries
 import webbrowser
 from bs4 import BeautifulSoup
 import urllib.request
@@ -11,8 +10,9 @@ def eatery():
   """
 
 #User input for search query
-  food_type = input("Hello! I am the restaurant_bot and I can recommend you a highly rated place to eat!\n\nI have only one rule: no spaces are allowed in your answers!
-                    \n\nWhat do you feel like eating? (Ex. Korean-BBQ) ")
+  food_type = input("Hello! I am the restaurant_bot and I can recommend you a highly rated place to eat!"
+                    "\n\nI have only one rule: no spaces are allowed in your answers!"
+                    "\n\nWhat do you feel like eating? (Ex. Korean-BBQ) ")
   location = input("\nWhere are you located? (Ex. San-Francisco) ")
   results = ('https://www.yelp.com/search?find_desc=' + food_type + '&find_loc=' + location + '&sortby=review_count')
  
